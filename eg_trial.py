1060f47b22910b01c3954975a36ff25258bd1317
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-01 16:33:20 +0530

    Examples Workout

---
+++ eg_trial.py
@@ -0,0 +1,21 @@
+first_name="James"
+last_name="Bond"
+name=first_name+last_name
+print(name)
+
+first_name="James"
+last_name="Bond"
+name=first_name+" "+last_name
+print(name)
+
+str_a="This is a longer string"
+print(str_a[1],str_a[2],str_a[10],str_a[16])
+
+age=5564
+print(type(age))
+
+print("hello")
+print("Python")
+
+print("Hello",end=" ")
+print("python")
