e2cabc290bd578e8e0c04240bc523a435b1a3f1a
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-01 15:20:21 +0530

    Document header added

--- student_details.py
+++ student_details.py
@@ -1,3 +1,11 @@
+#document header start
+'''
+Python program to get the student details
+Author: Anannya Abhi
+Date:01-10-2024
+Version: 1.0
+'''
+#document header end
 name=input('Enter the student name:')
 roll_number=int(input('Enter your roll number:'))
 roll_number=roll_number+1
