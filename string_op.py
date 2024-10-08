aca2513851f976b901860d3e5a682179ccf6347d
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-08 15:16:03 +0530

    Add files via upload

---
+++ string_op.py
@@ -0,0 +1,13 @@
+"""
+String operations using python
+Author: Anannya Abhi
+Date:08.10.2024
+"""
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+name=first_name+" "+ last_name
+print(name)
+length=len(first_name)
+print(length)
+extracted_name=name[:length]
+print(extracted_name)
