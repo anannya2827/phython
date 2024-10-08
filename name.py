eb4643d1664dbd985de82ba82013ac1bf4d2b7ca
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-08 16:28:35 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+"""
+Example workout using python
+Author: Anannya Abhi
+Date:08.10.2024
+"""
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Hello,",name,"! You are",age,"years old.")
