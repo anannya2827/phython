185847bfd96ac91ecea631b9329dc9a767012859
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-08 15:15:13 +0530

    Add files via upload

---
+++ Basic_op.py
@@ -0,0 +1,20 @@
+"""
+Simple desktop calculator using python
+Author: Anannya Abhi
+Date:08.10.2024
+"""
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+num3=float(input("Enter third number:"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+subtract=num2-num1
+print("The difference when num2 is subtracted from num1 is:",subtract)
+multiply=num1*num2
+print("The product of num1 and num2 is:",multiply)
+divide=num1/num2
+print("The quotient when num1 is divided by num2 is:",divide)
+mod=num1%num2
+print("The remainder when num1 is divided by num2 is:",mod)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2) is:",result)
