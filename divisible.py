aacd3ace76adaa9f82878e7f5041a6992ae48a9c
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 14:57:10 +0530

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,11 @@
+'''
+Desicion making statements in python
+Author: Anannya Abhi
+Date:15.10.24
+'''
+# Number divisibility by 6
+num=int(input("\nEnter a number:"))
+if (num%5)==0:
+    print("Divisible by 5")
+else:
+    print("Not divisible by 5")
