526ccb04dcc5330f6a175901831e504271c3582d
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 16:23:28 +0530

    Add files via upload

---
+++ sum_digits.py
@@ -0,0 +1,13 @@
+'''
+Python program to get sum of digits
+Author: Anannya Abhi
+Date:15.10.24
+'''
+num=int(input("Enter a number:"))
+sum=0
+while num>0:
+    remain=num%10
+    sum=remain+sum
+    num=num//10
+print(sum,"is the sum of digits.")
+
