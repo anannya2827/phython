5e3f47c141d23188bf8b8061ddcda646cb23a3ad
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 16:29:59 +0530

    Update sum_digits.py

--- sum_digits.py
+++ sum_digits.py
@@ -4,10 +4,11 @@ Author: Anannya Abhi
 Date:15.10.24
 '''
 num=int(input("Enter a number:"))
+num1=num
 sum=0
 while num>0:
     remain=num%10
     sum=remain+sum
     num=num//10
-print(sum,"is the sum of digits.")
+print(sum,"is the sum of digits of",num1)
 
