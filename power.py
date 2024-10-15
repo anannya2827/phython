50719b89132abae2e6fb3b075d2f47385c0c9830
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 15:10:16 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,9 @@
+'''
+Math module examples in python
+Author: Anannya Abhi
+'''
+import math
+num=int(input("Enter the number:"))
+print("Square root of",num,":",math.sqrt(num))
+print("Factorial of ",num,":",math.factorial(num))
+print( num,"raised to power of 2:",math.pow(num,2))
