b3a734f123a315090b248a9bba6832fbb88171b9
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 14:02:56 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,10 @@
+'''
+Python program to check whether number positive or not
+Author: Anannya Abhi
+'''
+number=int(input("Enter a number:"))
+if number>0:
+  print ("The given number:",number,"is positive")
+else:
+    print("The given number:",number,"is negative")
+
