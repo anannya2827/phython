288ba37ea245dfbb637c4e51823caef82dc7aa70
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 14:55:43 +0530

    Add files via upload

---
+++ license.py
@@ -0,0 +1,11 @@
+'''
+Desicion making statements in python
+Author: Anannya Abhi
+Date:15.10.24
+'''
+#Driver's license eligibility
+age=int(input("\nEnter your age:"))
+if age==16 or age>16:
+   print("Eligible for driver's license")
+else:
+    print("Not eligible for driver's licence")
