279ce4e446f7e22298b14d81759ab4f7f953018d
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 14:02:38 +0530

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,12 @@
+'''
+Operations in python
+Author: Anannya Abhi
+'''
+a=10
+b=5
+print("Sum:",a+b)
+print("Division:",a/b)
+print('Is a greater than b ?:',a>b)
+print('Are a and b equal?:',a==b)
+print("Logical AND:",a>b and a<b)
+print("Logical OR:",a>b or a<b)
