254bd705eaabaa4d27f03c6452d72127a4958f78
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 14:50:15 +0530

    Add files via upload

---
+++ large.py
@@ -0,0 +1,14 @@
+'''
+Sequence statements in python
+Author: Anannya Abhi
+Date:15.10.24
+'''
+#largest number
+num1=int(input("Enter a first number:"))
+num2=int(input("Enter a second number:"))
+if num1>num2:
+   print(num1,"is larger.")
+elif num2>num1:
+   print(num2,"is larger.")
+else:
+   print("Numbers are equal.")
