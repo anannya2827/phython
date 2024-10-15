3aa5e5fa146e23954f5f3ec799e2bcc32aad7846
anannya2827 <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-15 16:10:56 +0530

    Add files via upload

---
+++ odd_till_n.py
@@ -0,0 +1,10 @@
+'''
+Python program to generate odd numbers till n
+Author: Anannya Abhi
+Date:15.10.24
+'''
+limit=int(input("Enter a limit:"))
+odd_num=1
+while odd_num<=limit:
+   print(odd_num,"\t",end="")
+   odd_num+=2
