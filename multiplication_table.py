603ed8c73e7a0024f24a59f78beb408fb7bf38d6
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-29 15:32:01 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,11 @@
+'''
+Name:Anannya Abhi
+Date:29/10/2024
+Python program to print prime numbers till limit
+'''
+limit=int(input("Enter a limit:"))
+print(f"Multiplication of {limit} till 12 is:")
+for i in range(13):
+  product=i*limit
+  print(f"\n {i}*{limit}={product}")
+
