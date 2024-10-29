4fce6d898a9f2738fb62a16427116b568f404ad4
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-10-29 14:27:23 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,12 @@
+number=int(input("Enter a number"))
+isPrime=True
+for i in range(2,(number//2)+1):
+   if number%i==0:
+     isPrime=False
+     break
+if isPrime:
+      print(f"The given number {number} is prime")
+else:
+      print(f"The given number {number} is not prime")
+
+
