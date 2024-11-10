5d11617e67844e913773d8a8b57693686dee6993
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2024-11-10 20:29:37 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -6,7 +6,7 @@ Python program to count number of vowels in a string
 input_string=input("Enter a string:")
 vowels="AEOIUaeiou"
 vowel_count=0
-for char in input_string:
+for char in input_string:#used membership operator
     if char in vowels: #used membership operator
         vowel_count+=1
 print(f"The number of vowels in {input_string} is:{vowel_count}")
